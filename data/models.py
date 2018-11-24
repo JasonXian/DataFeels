8175b7188687154d6352f6ade18b12ffa18e5134
Jason Xian <j4xian@uwaterloo.ca>
2018-11-24 12:12:25 -0500

    basic recommend page complete

--- data/models.py
+++ data/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Data(models.Model):
     url = models.CharField(max_length = 200)
+    thumbnail = models.CharField(max_length = 200)
     title = models.CharField(max_length = 200)
     author = models.CharField(max_length = 200)
     date = models.CharField(max_length = 200)
--- data/views.py
+++ data/views.py
@@ -1,13 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+data = [
+        {
+            'url' : "https://www.google.ca",
+            'title' : "article 1",
+            'author' : "jason xian",
+            'date' : "today",
+            'topic' : "topic1",
+            "summary" : "some random text",
+            'sentiment_value' : 0.3,
+            'thumbnail' : "https://pbs.twimg.com/profile_images/832456134292180992/k_PQ_FYi_400x400.jpg"
+        },
+        {
+            'url' : "https://www.yahoo.ca",
+            'title' : "article 2",
+            'author' : "bryan au",
+            'date' : "today",
+            'topic' : "topic1",
+            "summary" : "some random text",
+            'sentiment_value' : 0.7,
+            'thumbnail' : "https://pbs.twimg.com/profile_images/832456134292180992/k_PQ_FYi_400x400.jpg"
+        },
+    ]
+
+dummyData = {
+    'topic1' : {
+        'data' : {
+            'topic': 'topic1',
+            'high' : data,
+            'average' : data,
+            'low': data
+        }
+    },
+    'topic2' : {
+        'data' : {
+            'topic': 'topic2',
+            'high' : data,
+            'average' : data,
+            'low' : data
+        }
+    }
+}
+
 def index(request):
     context = {'latest_question_list': ["1","2","3"]}
     return render(request, 'data/index.html', context)
 
 def recommend(request, topic):
-    return HttpResponse("You're looking at question %s." % topic)
+    context = dummyData[topic]
+    return render(request, 'data/recommend.html', context)
 
 def graph(request, topic):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % topic)
+    context = dummyData[topic]
+    return render(request, 'data/graph.html', context)
