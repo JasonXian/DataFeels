9618d704031c721aa57d581c856d00b2312f5823
Jason Xian <j4xian@uwaterloo.ca>
2018-11-24 16:16:01 -0500

    basic graph function in place with dummy data

--- DataFeels/urls.py
+++ DataFeels/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 
 urlpatterns = [
+    path('', lambda request: redirect('index', permanent=False)),
     path('data/', include('data.urls')),
     path('admin/', admin.site.urls),
 ]
--- data/views.py
+++ data/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from random import randint
+from copy import deepcopy
 
-data = [
+seed = [
         {
             'url' : "https://www.google.ca",
             'title' : "article 1",
@@ -9,7 +11,7 @@ data = [
             'date' : "today",
             'topic' : "topic1",
             "summary" : "some random text",
-            'sentiment_value' : 0.3,
+            'sentiment_score' : 0.3,
             'thumbnail' : "https://pbs.twimg.com/profile_images/832456134292180992/k_PQ_FYi_400x400.jpg"
         },
         {
@@ -19,26 +21,37 @@ data = [
             'date' : "today",
             'topic' : "topic1",
             "summary" : "some random text",
-            'sentiment_value' : 0.7,
+            'sentiment_score' : 0.7,
             'thumbnail' : "https://pbs.twimg.com/profile_images/832456134292180992/k_PQ_FYi_400x400.jpg"
         },
     ]
+
+data = []
+
+for i in range(0, 50):
+    base = deepcopy(seed[randint(0, 1)])
+    score = randint(0,100) / 100
+    base["sentiment_score"] = score
+    base["title"] = "article " + str(i)
+    data.append(base)
 
 dummyData = {
     'topic1' : {
         'data' : {
             'topic': 'topic1',
-            'high' : data,
-            'average' : data,
-            'low': data
+            'high' : data[6:9],
+            'average' : data[3:6],
+            'low': data[0:3],
+            'all': data
         }
     },
     'topic2' : {
         'data' : {
             'topic': 'topic2',
-            'high' : data,
-            'average' : data,
-            'low' : data
+            'high' : data[15:18],
+            'average' : data[12:15],
+            'low': data[9:12],
+            'all' : data,
         }
     }
 }
@@ -52,5 +65,18 @@ def recommend(request, topic):
     return render(request, 'data/recommend.html', context)
 
 def graph(request, topic):
-    context = dummyData[topic]
+    sortedArr = [[0 for col in range(20)] for row in range(10)]
+    for article in dummyData[topic]['data']['all']:
+        index = round(article['sentiment_score'] * 20)
+        if index == 20:
+            index = 19
+        for row in sortedArr:
+            if row[index] == 0:
+                row[index] = article 
+                break
+    sortedArr.reverse()
+    context = {
+        'topic' : topic,
+        'sorted' : sortedArr,
+    }
     return render(request, 'data/graph.html', context)
